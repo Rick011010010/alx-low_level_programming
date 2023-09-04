066fcd964bde23cdc9eb3fe6ef1dce59fb77d8d3
elmyar ossama <oussama.elmyar92@gmail.com>
2023-09-04 09:24:15 -0700

    0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,35 +2,27 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - Read text file and print to STDOUT.
- * @file_name: text file being read
- * @num_letters: number of letters to be read
- * Return: bytes_written - actual number of bytes read and printed
- *         0 when function fails or file_name is NULL.
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
  */
-ssize_t read_textfile(const char *file_name, size_t num_letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-    char *buffer;
-    ssize_t file_descriptor;
-    ssize_t bytes_read;
-    ssize_t bytes_written;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-    file_descriptor = open(file_name, O_RDONLY);
-    if (file_descriptor == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-    buffer = malloc(sizeof(char) * num_letters);
-    if (buffer == NULL)
-    {
-        close(file_descriptor);
-        return (0);
-    }
-
-    bytes_read = read(file_descriptor, buffer, num_letters);
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
-    free(buffer);
-    close(file_descriptor);
-
-    return (bytes_written);
+	free(buf);
+	close(fd);
+	return (w);
 }
